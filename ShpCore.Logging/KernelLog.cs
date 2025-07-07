4137c40573b9bce313e2834e010049b0bd4b75c3
Iván Rodriguez <ivanrwcm25@gmail.com>
2025-07-07 04:46:56 -0300

    Logs info & func updated
    Signed-off-by: Iván Rodriguez <ivanrwcm25@gmail.com>

--- ShpCore.Logging/KernelLog.cs
+++ ShpCore.Logging/KernelLog.cs
@@ -10,12 +10,19 @@ public static class KernelLog
         Log.Logger = new LoggerConfiguration()
             .MinimumLevel.Debug()
             .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}")
-            .WriteTo.File("kernel.log", rollingInterval: RollingInterval.Day)
+             // .WriteTo.File("kernel.log", rollingInterval: RollingInterval.Day)
             .CreateLogger();
     }
 
+		// Necesary Info for events in runtime
     public static void Info(string msg) => Log.Information(msg);
+   
+   	// Util warns in development 
     public static void Warn(string msg) => Log.Warning(msg);
+   
+    // Fatal Errors
     public static void Panic(string msg, Exception? ex = null) => Log.Error(ex, msg);
+
+    // Runtime Data (most used)
     public static void Debug(string msg) => Log.Debug(msg);
 }
